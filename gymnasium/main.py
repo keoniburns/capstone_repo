71319209e7c7df3a23dce973b394d464f378d287
keoni Burns <keoni.burns@gmail.com>
2024-02-28 19:46:03 -0800

    gymnasium env set up

---
+++ gymnasium/main.py
@@ -0,0 +1,13 @@
+# python3
+
+import gym
+
+env = gym.make("Walker2d-v4")
+for _ in range(1000):
+    action = env.action_space.sample()  # this is where you would insert your policy
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
