519117aba1e90f3f43c0166dad3fd79ba31214ea
keoni Burns <keoni.burns@gmail.com>
2024-03-13 10:54:03 -0700

    stable baseline3

--- gymnasium/main.py
+++ gymnasium/main.py
@@ -1,13 +1,32 @@
-# python3
+# # python3
 
-import gym
+# import gymnasium as gym
 
-env = gym.make("Walker2d-v4")
-for _ in range(1000):
-    action = env.action_space.sample()  # this is where you would insert your policy
-    observation, reward, terminated, truncated, info = env.step(action)
+# env = gym.make("Walker2d-v4")
+# for _ in range(1000):
+#     action = env.action_space.sample()  # this is where you would insert your policy
+#     observation, reward, terminated, truncated, info = env.step(action)
 
-    if terminated or truncated:
-        observation, info = env.reset()
+#     if terminated or truncated:
+#         observation, info = env.reset()
 
-env.close()
+# env.close()
+
+import gymnasium as gym
+
+from stable_baselines3 import A2C
+
+env = gym.make("Walker2d-v4", render_mode="rgb_array")
+
+model = A2C("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=20_000)
+
+vec_env = model.get_env()
+obs = vec_env.reset()
+for i in range(1000):
+    action, _state = model.predict(obs, deterministic=True)
+    obs, reward, done, info = vec_env.step(action)
+    vec_env.render("human")
+    # VecEnv resets automatically
+    # if done:
+    #   obs = vec_env.reset()
