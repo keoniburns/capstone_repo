663fc5a0e0510e0fd4790581f82bb88c8e379d6c
keoni burns <keoni.burns@gmail.com>
2024-05-09 18:03:41 -0700

    added some stufffffs

--- gymnasium/config.py
+++ gymnasium/config.py
@@ -2,6 +2,8 @@ import os
 
 #**** Environment ********
 ENV = "Humanoid-v4"
+PATH_SAVE = "./new_models/"
+PATH_LOAD = None
 
 #******** Network ********
 
@@ -11,5 +13,20 @@ CRITIC_HIDDEN = [1024, 512]
 LOG_STD_MIN = -20 # exp(-10) = 4.540e-05
 LOG_STD_MAX = 2 # exp(2) = 7.389
 EPSILON = 1e-6
+
+#**** Replay Buffer ******
+MIN_SIZE_BUFFER = 100
+BUFFER_CAPACITY = 100000000
+BATCH_SIZE = 256
+
+
+#******* Training ********
+GAMMA = 0.99
+TAU = 0.005
+ACTOR_LR = 0.0005
+CRITIC_LR = 0.0005
+REWARD_SCALE = 2
+THETA = 0.15
+DT = 1e-1
 
 
--- gymnasium/main.py
+++ gymnasium/main.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append("../src")
-import gym # Import OpenAI gym
+import gymnasium as gym # Import OpenAI gym
 # import pybullet_envs # py
 import tensorflow as tf
 import numpy as np
@@ -11,6 +11,8 @@ from replay_buffer import *
 from networks import *
 from agent import *
 import gymnasium as gym
+import time
+import argparse
 
 from stable_baselines3 import SAC
 
@@ -29,28 +31,106 @@ from stable_baselines3 import SAC
 #     if done:
 #         obs = vec_env.reset()
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--env", default="Walker2d-v4")
+parser.add_argument("-t", "--train", action="store_true")
+parser.add_argument("-e", "--evaluate", metavar="path_to_model")
+args = parser.parse_args()
+
+print(ACTOR_LR, CRITIC_LR, BATCH_SIZE)
 config = dict(
-    learning_rate_actor = 0.0005,
-    learning_rate_critic = 0.0005,
-    batch_size = 64,
-    arch = "SAC",
+    learning_rate_actor=ACTOR_LR,
+    learning_rate_critic=CRITIC_LR,
+    batch_size=BATCH_SIZE,
+    arch="SAC",
+    env=args.env,
 )
 
 wandb.init(
-  project=f"tensorflow2_sac_{ENV_NAME.lower()}",
+  project=f"tensorflow2_sac_{args.env.lower()}",
   tags=["SAC", "FCL", "RL"],
   config=config,
 )
 
+env = gym.make(args.env)
+# replay_buffer = ReplayBuffer(buffer_size=BUFFER_SIZE)
+agent = Agent(env)
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--env", default="Pendulum-v0")
-    parser.add_argument("-t", "--train", action="store_true")
-    parser.add_argument("-e", "--evaluate", metavar="path_to_model")
-    args = parser.parse_args()
+
+    scores = []
+    evaluation = True
 
     if args.train:
-        env = gym.make(args.env)
-        agent = SACAgent(env, wandb.config)
-        agent.train()
-        agent.save()
+
+        for _ in tqdm(range(MAX_GAMES)):
+            # start_time = time.time()
+            # env = gym.make(args.env)
+            # agent = Agent(env)
+            # env = MonitorStateWrapper(env)  # Wrap the environment to record state transitions
+            # states = env.reset()
+            # done = False
+            # score = 0
+            #
+            # while not done:
+            #     action = agent.get_action(states)
+            #     new_states, reward, done, info = env.step(action)
+            #     score += reward
+            #
+            #     agent.replay_buffer.append((states, action, reward, new_states, done)) 
+            #     agent.learn() 
+            #     self.replay_buffer.add(state[0], action[0], reward[0], next_statei[0], done[0])
+            #     states = new_states
+            #
+            # scores.append(score)
+            #
+            # wandb.log({'Game number': _, 'Average reward': round(np.mean(scores[-10:]), 2),
+            #            "Time taken": round(time.time() - start_time, 2)})
+            #
+            # if (_ + 1) % SAVE_FREQUENCY == 0:
+            #     print("saving...")
+            #     agent.save()
+            #     print("saved")
+                # for _ in tqdm(range(MAX_GAMES)):
+            start_time = time.time()
+            states = env.reset()
+            done = False
+            score = 0
+            while not done:
+                action = agent.get_action(states)
+                new_states, reward, done, info = env.step(action)
+                score += reward
+                agent.add_to_replay_buffer(states, action,
+                                           reward, new_states, done)
+                agent.learn()
+                states = new_states
+            scores.append(score)
+            agent.replay_buffer.update_n_games()
+
+            wandb.log({'Game number': agent.replay_buffer.n_games,
+                       '# Episodes': agent.replay_buffer.buffer_counter,
+                       "Average reward": round(np.mean(scores[-10:]), 2), \
+                       "Time taken": round(time.time() - start_time, 2)})
+    else:
+        PATH_LOAD = args.evaluate
+        observation = env.reset()
+        action, log_probs = agent.actor.get_action_log_probs(observation[None, :], False)
+        agent.actor(observation[None, :])
+        agent.critic_0(observation[None, :], action)
+        agent.critic_1(observation[None, :], action)
+        agent.critic_value(observation[None, :])
+        agent.critic_target_value(observation[None, :])
+        agent.load()
+    #     env = gym.make(args.env)
+    #     agent = Agent(env)
+    #     for _ int tdqm(range(MAX_EPISODES)):
+    #         total_reward = 0
+    #         while not done:
+    #             action = agent.get_action(state)
+    #             next_state, reward, done, info = env.step(action)
+    #
+    #             agent.replay_buffer.add(state, action, reward, next_state, done)
+    #             state = next_state
+    #             total_reward += reward
+    #         agent.learn()
+    #         wandb.log({"total_reward": total_reward})
